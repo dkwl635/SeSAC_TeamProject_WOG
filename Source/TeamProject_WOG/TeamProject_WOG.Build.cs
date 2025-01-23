7017219dfe182695ef430667b34fac228f8d608e
SeSAcPc_dkwl635 <dkwl635@naver.com>
2025-01-23 11:08:36 +0900

    + 기본 프로젝트

---
+++ Source/TeamProject_WOG/TeamProject_WOG.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TeamProject_WOG : ModuleRules
+{
+	public TeamProject_WOG(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
