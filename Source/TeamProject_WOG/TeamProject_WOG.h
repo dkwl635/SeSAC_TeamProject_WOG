3fd0224e8e926e550e6111d92d228ede47ec60de
SeSAcPc_dkwl635 <dkwl635@naver.com>
2025-02-03 16:57:20 +0900

    콜리전 및 트레이스채널 추가

--- Source/TeamProject_WOG/TeamProject_WOG.h
+++ Source/TeamProject_WOG/TeamProject_WOG.h
@@ -1,5 +1,25 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "Engine/EngineTypes.h" 
+
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+namespace EKCollisionProfileName
+{
+	static const FName Player = FName("Player");
+	static const FName Enemy = FName("Enemy");
+	static const FName PlayerAttack = FName("PlayerAttack");
+	static const FName EnemyAttack = FName("EnemyAttack");
+}
+
+enum class ETraceChannel : uint8
+{
+	PlayerAttackTrace = ECC_GameTraceChannel5 ,
+	EnemyAttackTrace = ECC_GameTraceChannel6
+};
