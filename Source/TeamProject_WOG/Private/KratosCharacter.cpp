aa551c484d0ffd48d7959d5657844fe923f110b5
HyunSunAhn <kiwibird0417@gmail.com>
2025-02-04 16:20:54 +0900

    C++ 이동(블루프린트에서 변환)

--- Source/TeamProject_WOG/Private/KratosCharacter.cpp
+++ Source/TeamProject_WOG/Private/KratosCharacter.cpp
@@ -1,7 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "KratosCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
 
 // Sets default values
 AKratosCharacter::AKratosCharacter()
@@ -9,6 +13,26 @@ AKratosCharacter::AKratosCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// TPS 카메라를 붙이고 싶다.
+	// SpringArm 컴포넌트 붙이기
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	springArmComp->SetupAttachment(RootComponent);
+	springArmComp->SetRelativeLocation(FVector(0.0f , 60.0f , 80.0f));
+	springArmComp->TargetArmLength = 300.0f;
+
+	//카메라 회전(02/04(화))
+	springArmComp->bUsePawnControlRotation = true;
+
+	// Camera 컴포넌트 붙이기
+	KratosCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("KratosCamComp"));
+	KratosCamComp->SetupAttachment(springArmComp);
+
+	//카메라 회전(02/04(화))
+	KratosCamComp->bUsePawnControlRotation = false;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +40,25 @@ void AKratosCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	auto pc = Cast<APlayerController>(Controller);
+	if ( pc ) {
+		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+
+		if ( subsystem ) {
+			subsystem->AddMappingContext(IMC_Kratos , 0);
+		}
+	}
 }
 
 // Called every frame
 void AKratosCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	// 크레토스 이동
+	Direction = FTransform(GetControlRotation()).TransformVector(Direction);
+	AddMovementInput(Direction);
+	Direction = FVector::ZeroVector;
 
 }
 
@@ -30,5 +67,36 @@ void AKratosCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	//Cast Vs. CastChecked: null이면 강제 종료.
+	auto PlayerInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if ( PlayerInput )
+	{
+		PlayerInput->BindAction(IA_Turn , ETriggerEvent::Triggered , this , &AKratosCharacter::Turn);
+		PlayerInput->BindAction(IA_LookUp , ETriggerEvent::Triggered , this , &AKratosCharacter::LookUp);
+		PlayerInput->BindAction(IA_Move , ETriggerEvent::Triggered , this , &AKratosCharacter::Move);
+	}
+
+}
+
+void AKratosCharacter::Turn(const FInputActionValue& inputValue) {
+	float value = inputValue.Get<float>();
+	AddControllerYawInput(value);
+}
+
+void AKratosCharacter::LookUp(const FInputActionValue& inputValue) {
+	float value = inputValue.Get<float>();
+	AddControllerPitchInput(value);
+}
+
+void AKratosCharacter::Move(const FInputActionValue& inputValue)
+{
+	FVector2D value = inputValue.Get<FVector2D>();
+
+	// 상하 입력 처리
+	Direction.X = value.X;
+
+	// 좌우 입력 처리
+	Direction.Y = value.Y;
 }
 
--- Source/TeamProject_WOG/Public/KratosCharacter.h
+++ Source/TeamProject_WOG/Public/KratosCharacter.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputActionValue.h"
 #include "KratosCharacter.generated.h"
 
 UCLASS()
@@ -25,5 +26,43 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+// 2. Camera
+public:
+	UPROPERTY( EditDefaultsOnly , Category = Camera)
+	class USpringArmComponent* springArmComp;
+
+	UPROPERTY(EditAnywhere, Category = Camera)
+	class UCameraComponent* KratosCamComp;
+	
+// Camera Input
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* IMC_Kratos;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_LookUp;
+
+	UPROPERTY( EditDefaultsOnly , Category = "Input" )
+	class UInputAction* IA_Turn;
+
+	void Turn(const FInputActionValue& inputValue);
+
+	void LookUp(const FInputActionValue& inputValue);
+
+// 3. Movement Input
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Move;
+
+	// 이동 속도
+	UPROPERTY(EditAnywhere, Category = PlayerSettings)
+	float WalkSpeed = 600.0f;
+
+	// 이동 방향
+	FVector Direction;
+
+	void Move(const FInputActionValue& inputValue);
+
 
 };
