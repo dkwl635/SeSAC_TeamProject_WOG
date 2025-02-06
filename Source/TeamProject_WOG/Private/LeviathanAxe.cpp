d3e2afb3e86abdabc431ec0790064c60fd84c433
HyunSunAhn <kiwibird0417@gmail.com>
2025-02-06 19:09:15 +0900

    애니메이션 에셋 및 InputAction 추가(Return(R), Aim(Cntrl))

---
+++ Source/TeamProject_WOG/Private/LeviathanAxe.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LeviathanAxe.h"
+#include "Components/BoxComponent.h"
+
+ALeviathanAxe::ALeviathanAxe()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComp->SetupAttachment(RootComponent);
+
+}
+
+void ALeviathanAxe::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ALeviathanAxe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/TeamProject_WOG/Public/LeviathanAxe.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "LeviathanAxe.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMPROJECT_WOG_API ALeviathanAxe : public AWeapon
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ALeviathanAxe();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class USceneComponent* RootComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* MeshComp;
+	
+};
